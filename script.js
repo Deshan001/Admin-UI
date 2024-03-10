79c4b089fb0ce85fdf10861b0c35335558a741c5
Deshan001 <venomassassin01@gmail.com>
2024-03-10 14:59:42 +0530

    script updated

--- script.js
+++ script.js
@@ -28,6 +28,25 @@ menuBar.addEventListener('click', function () {
 
 
 
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+    if(window.innerWidth < 576) {
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show')) {
+            searchButtonIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+})
+
+
+
+
 
 if(window.innerWidth < 768) {
     sidebar.classList.add('hide');
